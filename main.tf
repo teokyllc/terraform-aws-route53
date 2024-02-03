f75697e4b425fda39acc8123ba587d87f949387f
Allan Taylor <allan@startmail.com>
2024-02-03 13:36:38 -0500

    wip

--- main.tf
+++ main.tf
@@ -21,17 +21,3 @@ resource "aws_route53_record" "records" {
   ttl      = each.value["ttl"]
   records  = each.value["records"]
 }
-
-resource "aws_route53_record" "alias_records" {
-  for_each = var.alias_records
-  name     = each.value["name"]
-  type     = each.value["type"]
-  ttl      = each.value["ttl"]
-  zone_id  = var.zone_id
-
-  alias {
-    name                   = each.value["alias_name"]
-    zone_id                = each.value["alias_zone_id"]
-    evaluate_target_health = each.value["alias_evaluate_target_health"]
-  }
-}
--- outputs.tf
+++ outputs.tf
@@ -9,11 +9,3 @@ output "zone_arn" {
 output "zone_id" {
   value = var.create_dns_zone ? aws_route53_zone.zone[0].zone_id : null
 }
-
-output "records" {
-  value = var.records ? aws_route53_record.records: null
-}
-
-output "alias_records" {
-  value = var.alias_records ? aws_route53_record.alias_records: null
-}
